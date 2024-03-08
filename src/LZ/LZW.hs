52e5e1bb5851802a803b56eeb2af8964224a017b
Jylen02 <saelenjere@cy-tech.fr>
2024-03-08 15:21:42 +0100

    added uncompressed

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -3,7 +3,7 @@
   Description : An implementation of LZW method
   Maintainer  : Jérémy SAELEN
 -}
-module LZ.LZW(compress, uncompress) where
+module LZ.LZW where
 
 import LZ.Dictionaries
 import Data.Maybe
@@ -12,7 +12,6 @@ import Data.List
 -- | LZW compress method
 compress :: String -> [Int]
 compress text = compressRec text ascii "" []
---compress _ = undefined
 
 compressRec :: String -> Dictionary -> String -> [Int] -> [Int]
 compressRec "" dict maxStr acc
@@ -32,7 +31,19 @@ compressRec text dict maxStr acc
 -- | LZW uncompress method
 -- If input cannot be uncompressed, returns `Nothing`
 uncompress :: [Int] -> Maybe String
-uncompress _ = undefined -- TODO
+uncompress encoded = uncompressRec encoded ascii (Just "")
 
 
-  
+uncompressRec :: [Int] -> Dictionary -> Maybe String -> Maybe String
+uncompressRec [] _ acc = acc
+
+uncompressRec encoded dict acc
+  | isNothing acc || length dict <= value = Nothing
+  | otherwise = uncompressRec (tail encoded) newDict (Just (fromJust acc ++ character))
+  where
+    value = head encoded
+    character = dict !! value
+    newDict = if length (fromJust acc) == 0 then dict else dict ++ [([lastCharacter acc]++[(head character)])]
+
+lastCharacter :: Maybe String -> Char
+lastCharacter str = (fromJust str) !! (length (fromJust str) - 1)
