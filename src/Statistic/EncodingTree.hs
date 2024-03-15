555bb38b1d8b78e2a90eb64f5cf266a0309eff3f
Nino Hamel <Nino.Hamel@Yahoo.fr>
2024-03-16 00:36:21 +0100

    ShannonFano first update
    code compile and tests doesn't end up in infinite loop

--- src/Statistic/EncodingTree.hs
+++ src/Statistic/EncodingTree.hs
@@ -1,12 +1,11 @@
 {- |
   Module : Statistic.EncodingTree
   Description : A module representing a binary tree for binary encoding
-  Maintainer : Robin Meneust, (Add the other authors name here)
+  Maintainer : ???
 -}
 module Statistic.EncodingTree(EncodingTree(..), isLeaf, count, has, encode, decodeOnce, decode, meanLength, compress, uncompress) where
 
 import Statistic.Bit
-
 import Data.Maybe
 
 data EncodingTree a = EncodingNode Int (EncodingTree a) (EncodingTree a)
@@ -24,61 +23,76 @@ count (EncodingLeaf cnt _  ) = cnt
 count (EncodingNode cnt _ _) = cnt
 
 -- | Search for symbol in encoding tree
+-- has: Recursively checks if a given symbol exists in the encoding tree.
 has :: Eq a => EncodingTree a -> a -> Bool
-(EncodingLeaf cnt a) `has` b = a == b
-(EncodingNode cnt left right) `has` b = left `has` b || right `has` b
-
+has (EncodingLeaf _ x) target = x == target
+has (EncodingNode _ left right) target = has left target || has right target
 
 -- | Computes the binary code of symbol using encoding tree
 -- If computation is not possible, returns `Nothing`.
+-- encode: Finds the binary code of a symbol by traversing the encoding tree. If the symbol is found, it returns the corresponding binary code; otherwise, it returns Nothing.
 encode :: Eq a => EncodingTree a -> a -> Maybe [Bit]
-encode tree symb
-	| tree `has` symb == False = Nothing -- If the symbol is not in the tree it's an error
-	| otherwise = encodeRec tree symb []
-
--- | Computes the binary code of symbol using encoding tree with an accumulator
--- If computation is not possible, returns `Nothing`.
-encodeRec :: Eq a => EncodingTree a -> a -> [Bit] -> Maybe [Bit]
-encodeRec (EncodingLeaf _ a) symb acc = if a == symb then Just acc else Nothing -- If we reached a leaf that corresponds to the symbol then we foudn the bits sequence and we return it, otherwise it's an error (it should not happen here)
-encodeRec (EncodingNode _ left right) symb acc
-	| left `has` symb = encodeRec left symb (acc++[Zero]) -- If the left tree has the symbol then we go left and we add a 0 to the bits sequence
-	| otherwise = encodeRec right symb (acc++[One]) -- Otherwise it means that it's either in the right tree or not in the tree. So we go right and we add a 1. If it's not in the right tree this function will return Nothing (look at the case with the EncodingLeaf)
+encode (EncodingLeaf _ _) _ = Just [Zero]  -- Return [Zero] for leaf nodes
+encode (EncodingNode _ left right) target
+    | has left target = case encode left target of
+                              Just bits -> Just (Zero : bits)
+                              Nothing -> Nothing
+    | has right target = case encode right target of
+                               Just bits -> Just (One : bits)
+                               Nothing -> Nothing
+    | otherwise = Nothing
 
 
 -- | Computes the first symbol from list of bits using encoding tree and also returns the list of bits still to process
 -- If computation is not possible, returns `Nothing`.
+-- decodeOnce: Decodes the first symbol from a list of bits using the encoding tree and returns the remaining bits. If decoding is not possible, it returns Nothing.
 decodeOnce :: EncodingTree a -> [Bit] -> Maybe (a, [Bit])
-decodeOnce (EncodingLeaf _ a) bits = Just (a, bits) -- It's a leaf so we found the symbol associated to the bits sequence, we can return it with the remaining bits sequence
-decodeOnce (EncodingNode _ left right) [] = Nothing -- We don't have any bits left to be read and we aren't in a leaf, it's an error
-decodeOnce (EncodingNode _ left right) (Zero:bits) = decodeOnce left bits -- We read a 0 so we go to the left node and we remove the bit read
-decodeOnce (EncodingNode _ left right) (One:bits) = decodeOnce right bits -- We read a 1 so we go to the right node and we remove the bit read
-decodeOnce _ _ = Nothing -- Error
+decodeOnce (EncodingLeaf _ x) bits = Just (x, bits)
+decodeOnce (EncodingNode _ left right) (bit:bits) =
+    if bit == Zero
+        then decodeOnce left bits
+        else decodeOnce right bits
+decodeOnce _ _ = Nothing
 
 -- | Computes list of symbols from list of bits using encoding tree
+-- decode: Decodes the entire list of symbols from a list of bits using the encoding tree. If decoding is not possible at any point, it returns Nothing.
 decode :: EncodingTree a -> [Bit] -> Maybe [a]
-decode tree bits = decodeRec tree bits []
+decode tree bits = decode' tree bits []
+    where
+        decode' _ [] acc = Just (reverse acc)
+        decode' _ _ [] = Nothing
+        decode' tree' bits' acc =
+            case decodeOnce tree' bits' of
+                Just (x, remainingBits) -> decode' tree remainingBits (x:acc)
+                Nothing -> Nothing  -- If decoding fails, return Nothing
 
--- | Computes list of symbols from list of bits using encoding tree with a accumulator
-decodeRec :: EncodingTree a -> [Bit] -> [a] -> Maybe [a]
-decodeRec tree [] acc = Just acc -- There are no remaining bit to be decoded
-decodeRec tree bits acc
-	| isNothing temp = Nothing -- If there is an error
-	| otherwise = decodeRec tree nextBits nextAcc -- There are still bits to be decoded
-	where
-		temp = decodeOnce tree bits -- temp is the symbol that we got from the sequence of bits
-		nextBits = if isNothing temp then [] else snd (fromJust temp) -- If there was an error nextBits is an empty list otherwise it's the remaining sequence of bits (we removed the bits read to decode the current symbol)
-		nextAcc = if isNothing temp then acc else acc++[fst (fromJust temp)] -- We add to the accumulator the new symbol. If there was an error, we keep the accumulator as it is
-	
 
 -- | Mean length of the binary encoding
 meanLength :: EncodingTree a -> Double
-meanLength _ = undefined -- TODO
+meanLength tree = fromIntegral totalLength / fromIntegral totalCount
+  where
+    (totalCount, totalLength) = traverseTree tree 0 0
+
+    traverseTree :: EncodingTree a -> Int -> Int -> (Int, Int)
+    traverseTree (EncodingLeaf cnt _) totalCnt totalLen = (totalCnt + cnt, totalLen + cnt)
+    traverseTree (EncodingNode cnt left right) totalCnt totalLen =
+      let (leftCnt, leftLen) = traverseTree left totalCnt totalLen
+          (rightCnt, rightLen) = traverseTree right totalCnt totalLen
+      in (totalCnt + cnt, totalLen + leftCnt + rightCnt)
 
 -- | Compress method using a function generating encoding tree and also returns generated encoding tree
+-- compress: Compresses a list of symbols using a provided encoder function that generates an encoding tree. It returns the generated encoding tree along with the compressed binary data.
 compress :: Eq a => ([a] -> Maybe (EncodingTree a)) -> [a] -> (Maybe (EncodingTree a), [Bit])
-compress _ _ = undefined -- TODO
+compress encoder input = case encoder input of
+                            Just tree -> let encoded = mapM (encode tree) input
+                                         in case encoded of
+                                              Just bits -> (Just tree, concat bits)
+                                              Nothing -> (Nothing, [])
+                            _ -> (Nothing, [])
 
 -- | Uncompress method using previously generated encoding tree
 -- If input cannot be uncompressed, returns `Nothing`
-uncompress :: (Maybe (EncodingTree a), [Bit]) -> Maybe [a]
-uncompress _ = undefined -- TODO
+-- uncompress: Uncompresses binary data using a provided encoding tree. If uncompression is not possible, it returns Nothing.
+uncompress :: Eq a => (Maybe (EncodingTree a), [Bit]) -> Maybe [a]
+uncompress (Just tree, bits) = decode tree bits
+uncompress _ = Nothing
--- src/Statistic/ShannonFano.hs
+++ src/Statistic/ShannonFano.hs
@@ -6,8 +6,38 @@
 module Statistic.ShannonFano(tree) where
 
 import Statistic.EncodingTree
+import Statistic.Source
+import Data.List (sortBy)
+import Data.Map(toList)
+import Data.Function (on)
+
+ 
 
 -- | Shannon-Fano tree generation
 tree :: Ord a => [a] -> Maybe (EncodingTree a)
-tree _ = undefined -- TODO
+tree [] = Nothing  -- Base case: Empty list
+tree symbols = buildTree $ sortBy (compare `on` snd) $ orderedProbabilities symbols
 
+-- | Calculate symbol probabilities
+probabilities :: Ord a => [a] -> [(a, Double)]
+probabilities symbols = map (\(x, cnt) -> (x, fromIntegral cnt / total)) counts
+  where
+    counts = toList $ occurrences symbols
+    total = fromIntegral $ sum $ map snd counts
+
+-- | Sort symbols by their probabilities
+orderedProbabilities :: Ord a => [a] -> [(a, Double)]
+orderedProbabilities = sortBy (compare `on` snd) . probabilities
+
+-- | Build Shannon-Fano tree recursively
+buildTree :: Ord a => [(a, Double)] -> Maybe (EncodingTree a)
+buildTree [] = Nothing  -- Base case: Empty list
+buildTree [(x, _)] = Just (EncodingLeaf 1 x)  -- Base case: Single symbol
+buildTree symbols = case (leftTree, rightTree) of
+  (Just left, Just right) -> Just (EncodingNode totalCount left right)
+  _ -> Nothing
+  where
+    (leftSymbols, rightSymbols) = splitAt (length symbols `div` 2) symbols
+    totalCount = sum $ map (\(_, cnt) -> round cnt) symbols
+    leftTree = buildTree leftSymbols
+    rightTree = buildTree rightSymbols
