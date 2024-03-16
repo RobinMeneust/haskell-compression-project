a0be4297a7e4423ee6faaff36f0469f7a50bf1c0
Jylen02 <saelenjere@cy-tech.fr>
2024-03-16 18:26:09 +0100

    added comments for LZW

--- src/LZ/LZW.hs
+++ src/LZ/LZW.hs
@@ -13,19 +13,25 @@ import Data.List
 compress :: String -> [Int]
 compress text = compressRec text ascii "" []
 
-compressRec :: String -> Dictionary -> String -> [Int] -> [Int]
+-- | LZW compress method with accumulator
+compressRec :: String   -- ^ Text to be compressed
+    -> Dictionary       -- ^ Dictionary used for the compression
+    -> String           -- ^ Current text section that is in the dictionary (if it's not then we add it to the dictionary and we reset this text section)
+    -> [Int]            -- ^ Accumulator, it corresponds to the compressed version of the text that has already been read
+    -> [Int]            -- ^ Compressed text
 compressRec "" dict maxStr acc
-  | maxStr == "" = acc
-  | otherwise = (acc ++ [fromJust previousIndex])
-  where 
-    previousIndex = findIndex (\x -> x==maxStr) dict
+    | maxStr == "" = acc -- It corresponds to the case where the original text is empty
+    | otherwise = (acc ++ [fromJust previousIndex]) -- It corresponds to the case where it is the end of the text
+    where 
+        previousIndex = findIndex (\x -> x==maxStr) dict
+
 compressRec text dict maxStr acc
-  |isNothing index = compressRec text (dict ++ [newStr]) "" (acc ++ [fromJust previousIndex])
-  |otherwise = compressRec (tail text) dict newStr acc
-  where
-    newStr = maxStr ++ [head text]
-    index = findIndex (\x -> x==newStr) dict
-    previousIndex = findIndex (\x -> x==maxStr) dict
+    |isNothing index = compressRec text (dict ++ [newStr]) "" (acc ++ [fromJust previousIndex]) -- If it's not already in dict we can add it
+    |otherwise = compressRec (tail text) dict newStr acc -- If it's in the dict we add the char to the string to be searched in the dict
+    where
+        newStr = maxStr ++ [head text]
+        index = findIndex (\x -> x==newStr) dict
+        previousIndex = findIndex (\x -> x==maxStr) dict
 
 
 
@@ -35,15 +41,25 @@ uncompress :: [Int] -> Maybe String
 uncompress encoded = uncompressRec encoded ascii (Just "") 0
 
 
-uncompressRec :: [Int] -> Dictionary -> Maybe String -> Int -> Maybe String
-uncompressRec [] dict acc _ = acc
+uncompressRec :: [Int]  -- ^ Compressed data to be uncompressed
+    -> Dictionary       -- ^ Dictionary used to uncompress te data
+    -> Maybe String     -- ^ Accumulator, it corresponds to the uncompressed version of the data that has already been read
+    -> Int              -- ^ Previous value
+    -> Maybe String     -- ^ Uncompressed data
+uncompressRec [] _ acc _ = acc
 
 uncompressRec (value:encoded) dict acc previousValue
-  | isNothing acc || length dict < value || value < 0 = Nothing
-  | otherwise = uncompressRec encoded newDict (Just (res ++ character)) value
-  where
-    character = if value < (length dict) then dict !! value else (dict !! previousValue)++(last [(dict !! previousValue)])
-    res = fromJust acc
-    lastCharacter = last res
-    newDict = if length res == 0 then dict else if value < (length dict) then dict ++ [([lastCharacter]++[(head character)])] else dict ++ [([lastCharacter]++[head character])] ++ [([lastCharacter]++(character))]
+    | isNothing acc || length dict < value || value < 0 = Nothing -- If it cannot be uncompressed
+    | otherwise = uncompressRec encoded newDict (Just (res ++ character)) value -- If it can be uncompressed
+    where
+        character = if value < (length dict) 
+                then dict !! value
+                else (dict !! previousValue) ++ (last [(dict !! previousValue)])
+        res = fromJust acc
+        lastCharacter = last res
+        newDict = if length res == 0 
+                then dict 
+                else if value < (length dict) 
+                    then dict ++ [([lastCharacter]++[(head character)])] 
+                    else dict ++ [([lastCharacter]++[head character])] ++ [([lastCharacter]++(character))]
 
